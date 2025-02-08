5cc2af3c24ed27a26f6cfa6bf4ef4d94f518d267
Murasamy <peiyang7@illinois.edu>
2025-02-08 14:10:23 -0600

    Update __init__.py

--- picar_4wd/__init__.py
+++ picar_4wd/__init__.py
@@ -90,6 +90,7 @@ current_angle = 0
 max_angle = ANGLE_RANGE/2
 min_angle = -ANGLE_RANGE/2
 scan_list = []
+distance_list = []
 
 errors = []
 
@@ -144,13 +145,38 @@ def scan_step(ref):
     elif current_angle <= min_angle:
         current_angle = min_angle
         us_step = STEP
-    status = get_status_at(current_angle, ref1=ref)#ref1
+    status = get_status_at(current_angle, ref1=ref) #ref1
 
     scan_list.append(status)
     if current_angle == min_angle or current_angle == max_angle:
         if us_step < 0:
             # print("reverse")
             scan_list.reverse()
+        # print(scan_list)
+        tmp = scan_list.copy()
+        scan_list = []
+        return tmp
+    else:
+        return False
+    
+# Peiyang Eddition
+def scan_step_distance():
+    global distance_list, current_angle, us_step
+    current_angle += us_step
+    # scan back and forth
+    if current_angle >= max_angle:
+        current_angle = max_angle
+        us_step = -STEP
+    elif current_angle <= min_angle:
+        current_angle = min_angle
+        us_step = STEP
+    distance = get_distance_at(current_angle)
+
+    distance_list.append(distance)
+    if current_angle == min_angle or current_angle == max_angle:
+        if us_step < 0:
+            # print("reverse")
+            distance_list.reverse()
         # print(scan_list)
         tmp = scan_list.copy()
         scan_list = []
