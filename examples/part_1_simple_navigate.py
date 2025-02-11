d35d010e7cc766210edd76bab39f7f565d90d54c
Murasamy <peiyang7@illinois.edu>
2025-02-11 11:38:32 -0600

    Update part_1_simple_navigate.py

--- examples/part_1_simple_navigate.py
+++ examples/part_1_simple_navigate.py
@@ -5,11 +5,12 @@ speed = 30
 
 def main():
     while True:
-        scan_list = fc.scan_step(35) # ref1=35: 35cm is the distance to return 2
-        if not scan_list:
+        # scan_list = fc.scan_step(35) # ref1=35: 35cm is the distance to return 2
+        distance_list = fc.scan_step_distance()
+        if not distance_list:
             continue
 
-        tmp = scan_list[3:7]
+        tmp = distance_list[3:7] # distance in front, uint: cm
         # print(tmp)
         # if tmp != [2,2,2,2]:
         #     fc.turn_right(speed)
@@ -21,7 +22,9 @@ def main():
         # if obstacle detected in front, deside to turn left or right
         else:
             distance_list = fc.scan_step_distance()
+            # detect whether to turn left or right
             
+
 
 
 if __name__ == "__main__":
